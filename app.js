b863217e66b355ba57bbd5b5e6297a9da542cc2c
dev <julien.mangeat@mail-formateur.net>
2023-03-17 10:39:36 +0100

    archi

--- app.js
+++ app.js
@@ -54,39 +54,39 @@ if (targetNode) {
 //     }
 
 // div main
-    // relative h-full w-full transition-width flex flex-col overflow-hidden items-stretch flex-1
+// relative h-full w-full transition-width flex flex-col overflow-hidden items-stretch flex-1
 // old div 
-    // overflow-hidden w-full h-full relative
+// overflow-hidden w-full h-full relative
 // div d'envoie
-    // absolute bottom-0 left-0 w-full border-t md:border-t-0 dark:border-white/20 md:border-transparent md:dark:border-transparent md:bg-vert-light-gradient bg-white dark:bg-gray-800 md:!bg-transparent dark:md:bg-vert-dark-gradient pt-2
+// absolute bottom-0 left-0 w-full border-t md:border-t-0 dark:border-white/20 md:border-transparent md:dark:border-transparent md:bg-vert-light-gradient bg-white dark:bg-gray-800 md:!bg-transparent dark:md:bg-vert-dark-gradient pt-2
 
-    function createPdf() {
-        // suppréssion de la zone d'envoie
-        removeElementsByClassName('absolute bottom-0 left-0 w-full border-t md:border-t-0 dark:border-white/20 md:border-transparent md:dark:border-transparent md:bg-vert-light-gradient bg-white dark:bg-gray-800 md:!bg-transparent dark:md:bg-vert-dark-gradient pt-2')
-        // const selector = "#elementId"; // Remplacez cette valeur par le sélecteur de l'élément que vous souhaitez imprimer.
-        // querySelector sur la partie balise main
-        const element = document.querySelector('main');
-        if (element) {
-            html2pdf().from(element).save();
-        } else {
-            alert("Élément non trouvé");
-        }
-    }
-
-    function loadScript(url, callback) {
-        const script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = url;
-        script.onload = callback;
-        document.head.appendChild(script);
-    }
-
-    if (typeof html2pdf === "undefined") {
-        const html2pdfUrl = "https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.min.js";
-        loadScript(html2pdfUrl, createPdf);
+function createPdf() {
+    // suppréssion de la zone d'envoie
+    removeElementsByClassName('absolute bottom-0 left-0 w-full border-t md:border-t-0 dark:border-white/20 md:border-transparent md:dark:border-transparent md:bg-vert-light-gradient bg-white dark:bg-gray-800 md:!bg-transparent dark:md:bg-vert-dark-gradient pt-2')
+    // const selector = "#elementId"; // Remplacez cette valeur par le sélecteur de l'élément que vous souhaitez imprimer.
+    // querySelector sur la partie balise main
+    const element = document.querySelector('main');
+    if (element) {
+        html2pdf().from(element).save();
     } else {
-        createPdf();
+        alert("Élément non trouvé");
     }
+}
+
+function loadScript(url, callback) {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = url;
+    script.onload = callback;
+    document.head.appendChild(script);
+}
+
+if (typeof html2pdf === "undefined") {
+    const html2pdfUrl = "https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.min.js";
+    loadScript(html2pdfUrl, createPdf);
+} else {
+    createPdf();
+}
 
 // remove barre envoie
     //absolute bottom-0 left-0 w-full border-t md:border-t-0 dark:border-white/20 md:border-transparent md:dark:border-transparent md:bg-vert-light-gradient bg-white dark:bg-gray-800 md:!bg-transparent dark:md:bg-vert-dark-gradient pt-2
