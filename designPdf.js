a900b4379d3adb9d5879080c445c390bdf17424d
dev <julien.mangeat@mail-formateur.net>
2023-03-18 08:25:54 +0100

    modification pour plien ecrant max-w-full

--- designPdf.js
+++ designPdf.js
@@ -18,7 +18,7 @@ function updateElementsOnMutation(mutationsList) {
             mutation.addedNodes.forEach((node) => {
                 if (node.nodeType === Node.ELEMENT_NODE && node.nodeName === 'DIV') {
                     updateElementsByClassSelector('div[class="flex h-full flex-1 flex-col md:pl-[260px]"]', 'flex h-full flex-1 flex-col');
-                    updateElementsByClassSelector('div[class="text-base gap-4 md:gap-6 md:max-w-2xl lg:max-w-2xl xl:max-w-3xl p-4 md:py-6 flex lg:px-0 m-auto"]', 'text-base gap-4 md:gap-6 lg:max-w-2xl xl:max-w-3xl p-4 md:py-6 flex lg:px-0');
+                    updateElementsByClassSelector('div[class="text-base gap-4 md:gap-6 md:max-w-2xl lg:max-w-2xl xl:max-w-3xl p-4 md:py-6 flex lg:px-0 m-auto"]', 'text-base gap-4 md:gap-6 lg:max-w-full xl:max-w-full p-4 md:py-6 flex lg:px-0');
                 }
             });
         }
@@ -27,7 +27,7 @@ function updateElementsOnMutation(mutationsList) {
 
 removeElementsByClassName("dark hidden bg-gray-900 md:fixed md:inset-y-0 md:flex md:w-[260px] md:flex-col");
 updateElementsByClassSelector('div[class="flex h-full flex-1 flex-col md:pl-[260px]"]', 'flex h-full flex-1 flex-col');
-updateElementsByClassSelector('div[class="text-base gap-4 md:gap-6 md:max-w-2xl lg:max-w-2xl xl:max-w-3xl p-4 md:py-6 flex lg:px-0 m-auto"]', 'text-base gap-4 md:gap-6 lg:max-w-2xl xl:max-w-3xl p-4 md:py-6 flex lg:px-0');
+updateElementsByClassSelector('div[class="text-base gap-4 md:gap-6 md:max-w-2xl lg:max-w-2xl xl:max-w-3xl p-4 md:py-6 flex lg:px-0 m-auto"]', 'text-base gap-4 md:gap-6 lg:max-w-full xl:max-w-full p-4 md:py-6 flex lg:px-0');
 
 // Sélectionner l'élément à observer
 const targetNode = document.querySelector('div[class="flex flex-col items-center text-sm dark:bg-gray-800"]');
